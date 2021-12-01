c0e700b810a8acf380eea97560c1edc338e9c792
Millie <millie_tsai@me.com>
2021-11-30 23:42:25 -0800

    updated delete

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipeRoutes = require('./recipeRoutes');
-const userRecipeRoutes = require('./userRecipeRoutes');
+
 const addRecipeRoutes = require ('./addReceipRoutes');
 
 
 
 router.use('/users', userRoutes);
 router.use('/recipe', recipeRoutes);
-router.use('/user-recipe', userRecipeRoutes);
 router.use('/add-recipe', addRecipeRoutes);
 
 
--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -16,21 +16,10 @@ router.get('/:id', async (req, res) => {
 
 // delete one recipe by its id
 router.delete ('/:id', async (req, res) => {
-  try {
-    const recipeData = await Recipe.destroy({where: {id:req.params.id}})
-    res.render('account-recipe', {logged_in: req.session.logged_in,
-      username: req.session.username}) 
-
-    if (!recipeData) {
-      res.status(404).json({ message: 'No recipe found with this id!' });
-      return;
-    }
-
-    res.status(200).json(recipeData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-
+  const recipeData = await Recipe.destroy({where: {id:req.params.id}})
+  const recipe = recipeData.get({ plain: true })
+  res.render('account-recipe', {recipe, logged_in: req.session.logged_in,
+    username: req.session.username}) 
 })
 
 module.exports = router;
