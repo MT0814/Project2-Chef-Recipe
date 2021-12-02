8a8805bc983c960bf70b0e7c4c4f3676c1b071d7
Millie <millie_tsai@me.com>
2021-12-01 17:48:09 -0800

    don't need to refresh the browser but need to delete the oldest recipe card first

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -3,41 +3,35 @@ const Recipe = require("../../models/Recipe");
 
 // get one recipe by its id
 router.get("/:id", async (req, res) => {
-  const recipeData = await Recipe.findByPk(req.params.id);
-  const recipe = recipeData.get({ plain: true });
-  res.render("recipe", {
-    recipe,
-    logged_in: req.session.logged_in,
-    username: req.session.username,
-  });
-});
+  try {
+    const recipeData = await Recipe.findByPk(req.params.id);
+    const recipe = recipeData.get({ plain: true });
+    res.render("recipe", {
+      recipe,
+      logged_in: req.session.logged_in,
+      username: req.session.username,
+    })
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
+})
 
 
 
 
 // delete one recipe by its id
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
-    const recipeData = await Recipe.destroy({
-      where: {
-        id: req.params.id
-      },
-    });
-
-    if (!projectData) {
-      res.status(404).json({ message: "No recipe found with this id!" });
-      return;
-    }
-
-    res.render("account-recipe", {
-      recipe,
+    const recipeData = await Recipe.destroy({ where: { id: req.params.id } })
+    res.render('account-recipe', {
       logged_in: req.session.logged_in,
-      username: req.session.username,
-    });
+      username: req.session.username
+    })
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 module.exports = router;
 
